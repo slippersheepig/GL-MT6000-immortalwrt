379b0b396dd38db5cdc4126921854bc8b7343a4b
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2026-05-04 18:51:42 +0800

    Modify default settings and update AdGuard Home
    Updated default IP, theme, and hostname. Added steps to update AdGuard Home to the latest version.

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,18 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Sheep-Router/g' package/base-files/files/bin/config_generate
+
+echo "==> 更新 adguardhome 到最新版本"
+rm -rf feeds/packages/net/adguardhome
+git clone --depth 1 https://github.com/kenzok8/openwrt-packages tmp-kenzo
+mv tmp-kenzo/adguardhome feeds/packages/net/
+rm -rf tmp-kenzo
+sed -i '\#files/adguardhome#d' feeds/packages/net/adguardhome/Makefile
+sed -i '\#\$(INSTALL_DIR) \$(1)/etc#d' feeds/packages/net/adguardhome/Makefile
